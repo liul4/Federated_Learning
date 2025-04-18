d33bfc3c045ebd32b802aec6179712c40fc1ed85
liul4 <liul4@tcd.ie>
2025-04-18 17:18:29 +0100

    add baseline

--- Baseline/imbalanced/net.py
+++ Baseline/imbalanced/net.py
@@ -38,6 +38,29 @@ class Net(nn.Module):
         return self.resnet(x)
 
 
+"""
+class CNN(nn.Module):
+    #Baseline CNN Model, adapted from https://github.com/adap/flower
+
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 61 * 61, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3) #output 3 classes
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 61 * 61)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+"""
+
+
 def get_weights(net):
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
